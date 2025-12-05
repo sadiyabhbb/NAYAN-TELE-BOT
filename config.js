1113eb373a64b538bf618aaa8f3581d0b73cb226
Mohammad Nayan <mohammadnayan1669@gmail.com>
2025-12-05 16:04:06 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ownerUsernames: ["MOHAMMADNAYAN"],//Your Username
+  ownerNumber: "+8801615298449",//Your Number
+  autoUpdate: false,
+  admin: ["5920596290"], // your id
+  prefix: "/", // dont change prefix 
+  telegramBotToken: "7693032113:AAF4irS4PwfR2Dgu9s5HBGxIPIAlQpBAsnk",//Your Bot Tokens
+  imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
+  port: process.env.PORT || 8053 || 8053, // port
+  mess: {
+    ownerlink: "https://t.me/MOHAMMADNAYAN",
+    eror: "Internal Server Eror 😵",
+    owner: "Sorry, this command can only be accessed by the owner!",
+    group: "Sorry, this command can only be used within a group!",
+    wait: "Your request is being processed...",
+  },
+};
