68d18437841607eb7d2e54392cbc5eec464d02cb
Mohammad Nayan <mohammadnayan1669@gmail.com>
2025-12-05 16:03:30 +0600

    Add files via upload

--- cmd/events/join.js
+++ cmd/events/join.js
@@ -1,1 +1,35 @@
+const axios = require("axios");
 
+module.exports = {
+  event: "new_chat_members",
+
+  handle: async ({ msg, bot, moments }) => {
+    try {
+      const chatId = msg.chat.id;
+      const newUser = msg.new_chat_members[0];
+
+      const timestamp = moments.tz("Asia/Dhaka").format("hh:mm:ss");
+      const joinDate = new Date(msg.date * 1000).toLocaleDateString();
+
+      const profile = await global.userInfo(newUser.username);
+
+      const imageUrl = profile?.image;
+
+      const img = await axios.get(imageUrl, { responseType: "arraybuffer" });
+     const photoBuffer = Buffer.from(img.data);
+
+      const welcomeMessage = `
+🎉 Hai ${newUser.username || newUser.first_name}! 🎉
+Welcome to the group ${msg.chat.title}.
+Hour: ${timestamp}
+Day: ${joinDate}
+Hope you enjoy your stay. 😊
+`;
+
+      await bot.sendPhoto(chatId, photoBuffer, { caption: welcomeMessage });
+
+    } catch (err) {
+      console.error("WELCOME ERROR:", err);
+    }
+  }
+};
---
+++ cmd/events/leave.js
@@ -0,0 +1,34 @@
+const axios = require("axios")
+
+module.exports = {
+  event: 'left_chat_member',
+  handle: async ({ msg, bot }) => {
+    const chatId = msg.chat.id;
+    const leftUser = msg.left_chat_member;
+
+    const profile = await global.userInfo(leftUser.username);
+
+    const imageUrl = profile?.image;
+    const img = await axios.get(imageUrl, { responseType: "arraybuffer" });
+     const photoBuffer = Buffer.from(img.data);
+
+    
+    const goodbyeGroupMessage = `
+👋 *A member has left the group*:
+It’s with a heavy heart that we bid farewell to ${leftUser.username} from the group *${msg.chat.title}*. 😢
+We hope they’ll return someday. Take care, ${leftUser.username}! 🌟
+`;
+
+ 
+    bot.sendPhoto(chatId, photoBuffer, { caption: goodbyeGroupMessage, parse_mode: "Markdown" });
+
+    
+    const goodbyeUserMessage = `
+👋 Goodbye, ${leftUser.username}! 😢
+You have left the group *${msg.chat.title}*. 
+We hope you had a great time with us and that you'll return one day! Take care, and we’ll miss you! ✨
+`;
+
+    bot.sendMessage(leftUser.id, goodbyeUserMessage, { parse_mode: "Markdown" });
+  },
+};
