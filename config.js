734dc5668fa7f25214e50e25f7eaac9396d5d5a4
sadiyabhbb <sadiyabhbb@proton.me>
2025-12-07 20:59:28 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  ownerUsernames: ["rx_rihad"],//Your Username
-  ownerNumber: "+8801761838316",//Your Number
+  ownerUsernames: ["rx_rihad"],
+  ownerNumber: "+8801761838316",
   autoUpdate: true,
-  admin: ["7933110913"], // your id
-  prefix: "/", // dont change prefix 
-  telegramBotToken: "8034233581:AAFySYKNpv2g2XFxc_UdygSOm8RTuyvDxz4",//Your Bot Tokens
-  imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
-  port: process.env.PORT || 8053 || 8053, // port
+  admin: ["7933110913"], 
+  prefix: "/", 
+  telegramBotToken: "8034233581:AAFySYKNpv2g2XFxc_UdygSOm8RTuyvDxz4",
+  imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",
+  port: process.env.PORT || 8053 || 8053, 
   mess: {
     ownerlink: "https://t.me/rx_rihad",
     eror: "Internal Server Eror 😵",
