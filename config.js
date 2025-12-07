cfa44c7d4a5e4fc2db91ba478768c1c2fee63c99
sadiyabhbb <sadiyabhbb@proton.me>
2025-12-07 08:34:18 +0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   ownerUsernames: ["rx_rihad"],//Your Username
   ownerNumber: "+8801761838316",//Your Number
   autoUpdate: true,
-  admin: ["57933110913"], // your id
+  admin: ["7933110913"], // your id
   prefix: "/", // dont change prefix 
   telegramBotToken: "8034233581:AAFySYKNpv2g2XFxc_UdygSOm8RTuyvDxz4",//Your Bot Tokens
   imageUrl: "https://i.postimg.cc/WpmJhNVg/received-383729254132460.jpg",//Thumbnail Url
